6eaa26deecbf186e5cb3bba9524b987f06c38a15
peddinasameer <peddinasameer@gmail.com>
2025-06-13 14:37:01 +0530

    fixes program creation error

--- src/main/resources/dbscripts/InsertDB_Program_type.sql
+++ src/main/resources/dbscripts/InsertDB_Program_type.sql
@@ -10,6 +10,32 @@ insert into program_type (program_type_id,program_type,agency_id) values(20,'Wor
 insert into program_type (program_type_id,program_type,agency_id) values(24,'ESDP Training',9),(25,'Awareness Program',9)
 insert into program_type (program_type_id,program_type,agency_id) values(26,'Awareness Program',10),(27,'Training',10),(28,'Event',10);
 insert into program_type (program_type_id,program_type,agency_id) values(29,'Awareness Program',11),(30,'Training',11),(31,'Event',11),(32,'RBSM',11);
+
+insert into program_type (program_type_id,program_type,agency_id) values
+(34,'Hiring a Technology Firm',8),(35,'Training Sessions on Thematic Area',8),(36,'BDS Mobilisation Drive and Empanelment',8),
+(37,'Central Data Repository',8),(38,'MSME Awareness & Outreach (Branding & PR)',8),(39,'Capacity Building for DICs & IAs on Market and Access to Credit',8),
+(40,'Capacity Building and Handholding for various Govt Schemes',8),(41,'Call Centre_Udhyog Sahayak',8);
+insert into program_type (program_type_id,program_type,agency_id) values(42,'Engagement of Staff',10),(43,'Capacity Building and Mobilisation',10);
+insert into program_type (program_type_id,program_type,agency_id) values(44,'Training nad Workshops',1),(45,'Corpus Fund',1);
+insert into program_type (program_type_id,program_type,agency_id) values(46,'Conducting Study & Dashboard',3),(47,'Operating Expenditure',3);
+insert into program_type (program_type_id,program_type,agency_id) values(48,'Project Strategy & Innovation',4),(49,'Capacity Building, Awareness, Technology Demonstration Program',4),
+(50,'Innovation in Clean Energy (Startup Grand Challenge)',4),(51,'Enabling Greening of MSMEs',4);
+insert into program_type (program_type_id,program_type,agency_id) values(52,'Preliminery Survey for Selection of Potential SHGs',9),
+(53,'Awareness Programs abt the project',9),(54,'15 days training (Hands on training) for SHGs / Startups',9),
+(55,'Training of MSME Counsellors',9),(56,'Dashboard / Central Management System',9),(57,'Handholding support for Enterprise Creation',9),
+(58,'Raw Material Support',9),(59,'Contingency Fund',9);
+insert into program_type (program_type_id,program_type,agency_id) values(60,'Engagement of Staff',7),
+(61,'Physical Workshops',7),(62,'Informal Eco System Meets',7),(63,'IEC Material',7),(64,'Single Window Platform',7),
+(65,'MooC Programs',7),(66,'Demo Day Presentation Training Programs',7),(67,'Overhead',7),(68,'Contingency',7);
+insert into program_type (program_type_id,program_type,agency_id) values(69,'IT Infrastructure Setup',2),
+(70,'Event Organisation',2),(71,'Engagement of Staff',2),(72,'Consumables',2),(73,'Travel and Transport',2);
+insert into program_type (program_type_id,program_type,agency_id) values(74,'Manpower Deployment',11),
+(75,'Creation of Knowledge Repository for 10 Export Oriented Sectors',11),(76,'Handholding Support to MSMEs',11),
+(77,'Awareness / Orientation / Training Programs',11),(78,'Admn Expenditure (Travel/admn)',11);
+insert into program_type (program_type_id,program_type,agency_id) values(79,'Sectoral Benchmarking to assess Technology Gaps',5);
+insert into program_type (program_type_id,program_type,agency_id) values(80,'Sectoral Benchmarking to assess Technology Gaps',6);
+
 insert into agency (agency_id, agency_name) values
 (1,'TiHCL_05'),(2,'CoDe_IITH'),(3,'RICH_6A'),(4,'RICH_6B'),
-(5,'CITD_11'),(6,'CIPET_!!'),(7,'WeHub'),(8,'NIMSME'),(9,'ALEAP');
+(5,'CITD_11'),(6,'CIPET_!!'),(7,'WeHub'),(8,'NIMSME'),(9,'ALEAP'),(10,'TGTPC-4'),(11,'TGTPC-10'),(12,'CoI_01');
+
