736a1644d2521c14ed4b93b9b087ec4f535eb8bf
peddinasameer <peddinasameer@gmail.com>
2025-07-14 13:36:59 +0530

    adding particpant import fixes

--- src/main/java/com/metaverse/workflow/districtswithmandals/repository/DistrictRepository.java
+++ src/main/java/com/metaverse/workflow/districtswithmandals/repository/DistrictRepository.java
@@ -3,5 +3,8 @@ package com.metaverse.workflow.districtswithmandals.repository;
 import com.metaverse.workflow.model.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DistrictRepository extends JpaRepository<District,Integer> {
+    List<District> findByDistrictName(String district);
 }
