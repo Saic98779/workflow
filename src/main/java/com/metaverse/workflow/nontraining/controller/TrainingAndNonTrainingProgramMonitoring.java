9e66e30ffdc1cbb7d100c0df3bb3d44a4fb7980d
saic98 <134575105+saic98@users.noreply.github.com>
2025-08-17 20:19:09 +0530

    Non Training and expenditure entities added

--- src/main/java/com/metaverse/workflow/nontraining/controller/TrainingAndNonTrainingProgramMonitoring.java
+++ src/main/java/com/metaverse/workflow/nontraining/controller/TrainingAndNonTrainingProgramMonitoring.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/TrainingAndNonTrainingProgram")
+@RequestMapping("/training-non-training/monitoring")
 public class TrainingAndNonTrainingProgramMonitoring {
 
     @Autowired
