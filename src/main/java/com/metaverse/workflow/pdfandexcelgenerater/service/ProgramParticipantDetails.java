176fc0a5c68a38b96c117dbb2da6c17717505484
=anji <anji@anji>
2025-09-23 21:48:21 +0530

    programs-participants pdf generator api

--- src/main/java/com/metaverse/workflow/pdfandexcelgenerater/service/ProgramParticipantDetails.java
+++ src/main/java/com/metaverse/workflow/pdfandexcelgenerater/service/ProgramParticipantDetails.java
@@ -1,6 +1,7 @@
 package com.metaverse.workflow.pdfandexcelgenerater.service;
 
 
+import com.lowagie.text.Font;
 import com.metaverse.workflow.expenditure.repository.BulkExpenditureTransactionRepository;
 import com.metaverse.workflow.expenditure.repository.ProgramExpenditureRepository;
 import com.metaverse.workflow.model.BulkExpenditureTransaction;
@@ -12,9 +13,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.hssf.usermodel.*;
 import org.springframework.stereotype.Service;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ProgramParticipantDetails {
@@ -98,4 +105,105 @@ public class ProgramParticipantDetails {
         workbook.close();
         ops.close();
     }
+
+    public void generateProgramsParticipantPdf(HttpServletResponse response, Long agencyId) throws IOException {
+        List<Program> programSet = (agencyId == -1)
+                ? programRepository.findAll()
+                : programRepository.findByAgencyAgencyId(agencyId);
+
+        Set<Program> programList = new LinkedHashSet<>(programSet);
+
+        Document document = new Document(PageSize.A4.rotate());
+        PdfWriter.getInstance(document, response.getOutputStream());
+        document.open();
+
+        // Title
+        Font titleFont = new Font(Font.HELVETICA, 14, Font.BOLD);
+        Paragraph title = new Paragraph("Program Participant Details", titleFont);
+        title.setAlignment(Element.ALIGN_CENTER);
+        title.setSpacingAfter(20f);
+        document.add(title);
+
+        // Headers
+        String[] headers = {
+                "Name of the IA", "Budget Head", "Name of the Program", "Total Participants", "Male",
+                "Female", "Trans Gender", "SC", "ST", "BC", "OC", "Minorities",
+                "Physically Challenged", "Total Expenditure"
+        };
+
+        PdfPTable table = new PdfPTable(headers.length);
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(10f);
+
+        float[] columnWidths = {2f, 2f, 4f, 2f, 1.5f, 1.5f, 1.5f, 1.5f, 1.5f, 1.5f, 1.5f, 1.5f, 2f, 2f};
+        table.setWidths(columnWidths);
+
+        Font headerFont = new Font(Font.HELVETICA, 10, Font.BOLD, Color.WHITE);
+        for (String header : headers) {
+            PdfPCell cell = new PdfPCell(new Phrase(header, headerFont));
+            cell.setBackgroundColor(Color.GREEN);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell.setPadding(5);
+            table.addCell(cell);
+        }
+
+        Font dataFont = new Font(Font.HELVETICA, 9);
+
+        // Data rows
+        for (Program program : programList) {
+            List<ProgramExpenditure> expenditureList =
+                    expenditureRepository.findByAgency_AgencyIdAndProgram_ProgramId(
+                            program.getAgency().getAgencyId(), program.getProgramId()
+                    );
+
+            List<BulkExpenditureTransaction> transactionList =
+                    transactionRepository.findByProgram_ProgramId(program.getProgramId());
+
+            table.addCell(new Phrase(program.getAgency().getAgencyName(), dataFont));
+            table.addCell(new Phrase(program.getProgramType(), dataFont));
+            table.addCell(new Phrase(program.getProgramTitle(), dataFont));
+
+            // Participant calculations
+            long total = program.getParticipants().size();
+            long male = program.getParticipants().stream()
+                    .filter(p -> "M".equalsIgnoreCase(String.valueOf(p.getGender()))).count();
+            long female = program.getParticipants().stream()
+                    .filter(p -> "F".equalsIgnoreCase(String.valueOf(p.getGender()))).count();
+            long trans = program.getParticipants().stream()
+                    .filter(p -> "T".equalsIgnoreCase(String.valueOf(p.getGender()))).count();
+
+            long sc = program.getParticipants().stream()
+                    .filter(p -> "sc".equalsIgnoreCase(p.getCategory())).count();
+            long st = program.getParticipants().stream()
+                    .filter(p -> "st".equalsIgnoreCase(p.getCategory())).count();
+            long bc = program.getParticipants().stream()
+                    .filter(p -> "bc".equalsIgnoreCase(p.getCategory())).count();
+            long oc = program.getParticipants().stream()
+                    .filter(p -> "oc".equalsIgnoreCase(p.getCategory())).count();
+            long minorities = program.getParticipants().stream()
+                    .filter(p -> "minorities".equalsIgnoreCase(p.getCategory())).count();
+            long disabled = program.getParticipants().stream()
+                    .filter(p -> "Y".equalsIgnoreCase(String.valueOf(p.getDisability()))).count();
+
+            double totalExpenditure = expenditureList.stream().mapToDouble(ProgramExpenditure::getCost).sum()
+                    + transactionList.stream().mapToDouble(BulkExpenditureTransaction::getAllocatedCost).sum();
+
+            // Add data to table
+            table.addCell(new Phrase(String.valueOf(total), dataFont));
+            table.addCell(new Phrase(String.valueOf(male), dataFont));
+            table.addCell(new Phrase(String.valueOf(female), dataFont));
+            table.addCell(new Phrase(String.valueOf(trans), dataFont));
+            table.addCell(new Phrase(String.valueOf(sc), dataFont));
+            table.addCell(new Phrase(String.valueOf(st), dataFont));
+            table.addCell(new Phrase(String.valueOf(bc), dataFont));
+            table.addCell(new Phrase(String.valueOf(oc), dataFont));
+            table.addCell(new Phrase(String.valueOf(minorities), dataFont));
+            table.addCell(new Phrase(String.valueOf(disabled), dataFont));
+            table.addCell(new Phrase(String.format("%.2f", totalExpenditure), dataFont));
+        }
+
+        document.add(table);
+        document.close();
+    }
+
 }
