56614030cec5d7536c3816e279e4b2546f82998e
saic98 <134575105+saic98@users.noreply.github.com>
2025-11-23 21:07:49 +0530

    All financial year changes for output progress

--- src/main/java/com/metaverse/workflow/config/OpenApiConfig.java
+++ src/main/java/com/metaverse/workflow/config/OpenApiConfig.java
@@ -9,6 +9,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+// We'll use fully-qualified model classes below to avoid name clashes with the annotation types.
 
 @Configuration
 @OpenAPIDefinition(
@@ -42,4 +48,19 @@ import org.springframework.context.annotation.Configuration;
         in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
+
+    // Expose a programmatic OpenAPI bean so we can inject build-time info from resources.
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${build.time:unknown}") String buildTime) {
+        // Build a description/version that includes the build timestamp.
+        String buildNote = "(build: " + buildTime + ")";
+
+        io.swagger.v3.oas.models.info.Info info = new io.swagger.v3.oas.models.info.Info()
+                .title("Workflow API")
+                .version("1.0 " + buildNote)
+                .description("Workflow API Documentation " + buildNote)
+                .contact(new io.swagger.v3.oas.models.info.Contact().name("Metaverse").email("support@metaverse.com"));
+
+        return new OpenAPI().info(info);
+    }
 }
