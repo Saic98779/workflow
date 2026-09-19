e330dbe8d6a02b8c24e1b50c2aa64c9490ea7552
saic98 <134575105+saic98@users.noreply.github.com>
2026-09-19 17:37:47 +0530

    repo issue

--- src/main/java/com/metaverse/workflow/expenditure/repository/BulkExpenditureTransactionRepository.java
+++ src/main/java/com/metaverse/workflow/expenditure/repository/BulkExpenditureTransactionRepository.java
@@ -1,7 +1,11 @@
 package com.metaverse.workflow.expenditure.repository;
 
 import com.metaverse.workflow.model.BulkExpenditureTransaction;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,5 +18,6 @@ public interface BulkExpenditureTransactionRepository extends JpaRepository<Bulk
     List<BulkExpenditureTransaction> findByProgram_ProgramIdIn(List<Long> programId);
     List<BulkExpenditureTransaction> findByExpenditure_BulkExpenditureId(Long bulkExpenditureId);
 
-    Optional<BulkExpenditureTransaction> findByIdForUpdate(Long transactionId);
+    @Query("select t from BulkExpenditureTransaction t where t.id = :transactionId")
+    Optional<BulkExpenditureTransaction> findByIdForUpdate(@Param("transactionId") Long transactionId);
 }
